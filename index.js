e76e5c9645aa82576b3662bf1e52df425ade7d25
Joshua Barrios <jgb16@my.fsu.edu>
2017-08-09 17:27:19 -0400

    New divs

--- index.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    var heights = $(".row").map(function() {
-        return $(this).height();
-    }).get(),
-
-    maxHeight = Math.max.apply(null, heights);
-
-    $(".row").height(maxHeight);
-});
